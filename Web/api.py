297bc7fb28907dccf25f6bf3631de49b220cbcf8
Moon Yong Joon <myjlms99@gmail.com>
2023-11-05 22:39:38 +0900

    Fastapi 처음 작동

--- Web/api.py
+++ Web/api.py
@@ -1,5 +1,6 @@
 
 from fastapi  import FastAPI
+from todo import todo_router
 
 app = FastAPI()
 
@@ -7,3 +8,5 @@ app = FastAPI()
 async def welcome() -> dict :
     return { "message" : "Hello World",
              "message1" : "그러니까"}
+
+app.include_router(todo_router)
