29d6cb23c87356a5f16ecefd4c7b596297849863
Moon Yong Joon <myjlms99@gmail.com>
2023-11-06 17:50:43 +0900

    간단한 웹 처리까지만

---
+++ app/internal/admin.py
@@ -0,0 +1,7 @@
+app.include_router(
+    admin.router,
+    prefix="/admin",
+    tags=["admin"],
+    dependencies=[Depends(get_token_header)],
+    responses={418: {"description": "I'm a teapot"}},
+)
